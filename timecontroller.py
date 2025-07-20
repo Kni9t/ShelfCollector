514c0f6f38365ae12df558ec333b65368e13378e
Kni9t <clone111@bk.ru>
2025-07-20 14:25:33 +0300

    increase parsing time

--- timecontroller.py
+++ timecontroller.py
@@ -7,7 +7,7 @@ class TimeController:
     def CalculationWaitTime(self):
         now = datetime.now()
         next_day = now + timedelta(days = 1)
-        next_time = next_day.replace(hour = 18, minute = 0, second = 0, microsecond = 0)
+        next_time = next_day.replace(hour = 19, minute = 0, second = 0, microsecond = 0)
 
         sleep_seconds = int((next_time - now).total_seconds()) + 5
         
