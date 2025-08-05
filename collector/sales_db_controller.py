8dcd3d38b599eda3b53fe1cc2a82b12f4094b6e4
Kni9t <clone111@bk.ru>
2025-08-05 13:35:57 +0300

    add func for work with markets hash

--- collector/sales_db_controller.py
+++ collector/sales_db_controller.py
@@ -23,7 +23,7 @@ class DBController():
             self.cursor.execute("INSERT INTO shelves (name) VALUES ('Лисья полка');")
             self.connection.commit()
             
-        cmd = f"CREATE TABLE IF NOT EXISTS markets (market_id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT, start_date TEXT, end_date TEXT, location TEXT)"
+        cmd = f"CREATE TABLE IF NOT EXISTS markets (market_id INTEGER PRIMARY KEY AUTOINCREMENT, hash TEXT, name TEXT, start_date TEXT, end_date TEXT, location TEXT)"
         self.cursor.execute(cmd)
         
         cmd = f"CREATE TABLE IF NOT EXISTS market_sales (id INTEGER PRIMARY KEY AUTOINCREMENT, market_id INTEGER, date TEXT, time TEXT, revenue INTEGER, cash TEXT, sender_id, sender_name, FOREIGN KEY(market_id) REFERENCES markets(id))"
@@ -41,8 +41,8 @@ class DBController():
         
     def AddMarkets(self, dataDict):
         for line in dataDict:
-            cmd = f"INSERT INTO markets (name, start_date, end_date, location) VALUES (?, ?, ?, ?)"
-            self.cursor.execute(cmd, (str(line['name']), str(line['start_date']), str(line['end_date']), str(line['location'])))
+            cmd = f"INSERT INTO markets (hash, name, start_date, end_date, location) VALUES (?, ?, ?, ?)"
+            self.cursor.execute(cmd, (str(line['hash']), str(line['name']), str(line['start_date']), str(line['end_date']), str(line['location'])))
         
         self.connection.commit()
         
@@ -52,6 +52,30 @@ class DBController():
             self.cursor.execute(cmd, (int(line['market_id']), str(line['date']), str(line['time']), int(line['revenue']), str(line['cash']), str(line['sender_id']), str(line['sender_name'])))
         
         self.connection.commit()
+        
+    def CheckMarketsHash(self, hash):
+        marketsList = self.GetAllMarketsHash()
+        
+        for market in marketsList:
+            if (hash == market["hash"]):
+                return market
+        
+        return None
+        
+    def GetAllMarketsHash(self):
+        self.cursor.execute(f"SELECT id, hash, name FROM markets")
+        rows = self.cursor.fetchall()
+        
+        output = []
+    
+        for row in rows:
+            output.append({
+                "id": row[0],
+                "hash": row[1],
+                "name": row[2]
+            })
+            
+        return output
     
     def SendQuery(self, query):
         self.cursor.execute(query)
