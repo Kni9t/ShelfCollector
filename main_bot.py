95bb43d3b7e4ec6889a424f051bec26064feb66d
Kni9t <clone111@bk.ru>
2025-08-09 21:17:53 +0300

    add logger

--- main_bot.py
+++ main_bot.py
@@ -1,26 +1,42 @@
 import time
 import json
 import sys
+import logging
 
 from datetime import datetime
 
 from bots.informer_bot import InformerBot
 
 try:
-    with open('params/parameters.json') as file:
-        parametersDict = dict(json.load(file))
-        file.close()
-    
-    with open('params/buttons_list.json', encoding = 'utf-8') as file:
-        buttons_list = dict(json.load(file))
-        file.close()    
-    
+    logging.basicConfig(filename = 'bot.log', encoding = 'utf-8', level=logging.DEBUG, format='%(name)s: %(asctime)s - %(levelname)s - %(filename)s - %(module)s - %(lineno)d - %(message)s')
+    logger = logging.getLogger('main_bot_logger')
 except Exception as e:
-    err = f'{datetime.now()} - Ошибка при запуске бота! {e}'
+    err = f'{datetime.now()} - Ошибка при запуске логгера бота! {e}'
+    print(err)
         
     with open('bot_error_log.txt', 'a', encoding = 'utf-8') as file:
         file.write(err + '\n')
         file.close()
+    sys.exit(1)
+
+try:
+    fileName = 'params/parameters.json'
+    with open(fileName) as file:
+        parametersDict = dict(json.load(file))
+        file.close()
+    logger.debug(f'Файл {fileName} успешно загружен!')
+    
+    fileName = 'params/buttons_list.json'
+    with open(fileName, encoding = 'utf-8') as file:
+        buttons_list = dict(json.load(file))
+        file.close()
+    logger.debug(f'Файл {fileName} успешно загружен!')
+    
+except Exception as e:
+    err = f'Ошибка при загрузке параметров бота! {e}'
+    
+    logger.error(err)
+    print(err)
     sys.exit(1)
 
 TelegramBot = InformerBot(parametersDict, buttons_list)
@@ -30,9 +46,9 @@ while True:
         TelegramBot.Run()
         
     except Exception as e:
-        err = f'{datetime.now()} - Ошибка при работе бота! {e}'
+        err = f'Ошибка при работе бота! {e}'
         
-        with open('bot_error_log.txt', 'a', encoding = 'utf-8') as file:
-            file.write(err + '\n')
-            file.close()
+        logger.error(err)
+        print(err)
+        
         time.sleep(5)
