cc42358d8c1b6da384a93965b03b3b2c4b7be8b7
Kni9t <clone111@bk.ru>
2025-08-14 23:29:47 +0300

    add catch errors

--- collector/shelf_collector.py
+++ collector/shelf_collector.py
@@ -28,266 +28,286 @@ class ShelfCollector:
             self.js.writeData(bufDate)
     
     def CollectSalesPolks(self):
-        sender_email = 'shop@polkius.ru'
-        
-        mails = self._GetMessagesFromGmail(sender_email, 1)
-        
-        readyLines = []
-        
-        lastDate = None
-        
-        for mail in mails:
-            title = mail.get_subject()
+        try:
+            sender_email = 'shop@polkius.ru'
             
-            msg = f'Обработка письма: {title}'
+            mails = self._GetMessagesFromGmail(sender_email, 1)
             
-            print(msg)
-            self.logger.debug(msg)
+            readyLines = []
             
-            if not mail.html_part:
-                msg = f'Письмо: {title} не содержит HTML'
-                
-                print(msg)
-                self.logger.debug(msg)
-                continue
-            
-            html = mail.html_part.get_payload().decode(mail.html_part.charset)
-            soup = BeautifulSoup(html, 'html.parser')
-            tables = soup.find_all('table')
-            
-            if not tables:
-                msg = f'Нет таблиц в письме: {title}'
-                
-                print(msg)
-                self.logger.warning(msg)
-                continue
-            
-            rows = str(tables[9]).split('</tr>')
-            rows = rows[4:-2]
-            
-            oldPolksDate = dict(self.js.getData())["polks"]
-            
-            if (oldPolksDate == 'None'):
-                previousDate = datetime.strptime('2000-01-01', '%Y-%m-%d')
-            else:
-                previousDate = datetime.strptime(oldPolksDate, '%Y-%m-%d')
-                
             lastDate = None
             
-            for line in rows:
-                cleaned_text = re.sub(r'<.*?>', '', line).strip()
-                cleaned_text = re.sub(r'\s+', ' ', cleaned_text).strip()
+            for mail in mails:
+                title = mail.get_subject()
                 
-                spitedStr = cleaned_text.split(' ')
-                
-                if (re.search(r'\b(\d{2})\.(\d{2})\.(\d{4})\b', spitedStr[0])):
-                    date_obj = datetime.strptime(spitedStr[0], '%d.%m.%Y')
-                    lastDate = date_obj.strftime('%Y-%m-%d')
-                    continue
-                
-                if datetime.strptime(lastDate, '%Y-%m-%d') <= previousDate:
-                    continue
-                
-                name = ''
-                for namePart in spitedStr[0:-4:1]:
-                    name += namePart + ' '
-                
-                bufLine = self._createDict(
-                    shelf_id = 1,
-                    name = name,
-                    count = spitedStr[-2].replace(',000', ''),
-                    revenue = spitedStr[-1].replace(',', ''),
-                    date = lastDate,
-                )
-                
-                readyLines.append(bufLine)
-            
-            if (len(readyLines) == 0):
-                msg = f'Данные из письма: {title} уже содержатся в БД!'
+                msg = f'Обработка письма: {title}'
                 
                 print(msg)
                 self.logger.debug(msg)
-            else:
-                msg = f'Письмо: {title} обработано успешно!'
+                
+                if not mail.html_part:
+                    msg = f'Письмо: {title} не содержит HTML'
+                    
+                    print(msg)
+                    self.logger.debug(msg)
+                    continue
+                
+                html = mail.html_part.get_payload().decode(mail.html_part.charset)
+                soup = BeautifulSoup(html, 'html.parser')
+                tables = soup.find_all('table')
+                
+                if not tables:
+                    msg = f'Нет таблиц в письме: {title}'
+                    
+                    print(msg)
+                    self.logger.warning(msg)
+                    continue
+                
+                rows = str(tables[9]).split('</tr>')
+                rows = rows[4:-2]
+                
+                oldPolksDate = dict(self.js.getData())["polks"]
+                
+                if (oldPolksDate == 'None'):
+                    previousDate = datetime.strptime('2000-01-01', '%Y-%m-%d')
+                else:
+                    previousDate = datetime.strptime(oldPolksDate, '%Y-%m-%d')
+                    
+                lastDate = None
+                
+                for line in rows:
+                    cleaned_text = re.sub(r'<.*?>', '', line).strip()
+                    cleaned_text = re.sub(r'\s+', ' ', cleaned_text).strip()
+                    
+                    spitedStr = cleaned_text.split(' ')
+                    
+                    if (re.search(r'\b(\d{2})\.(\d{2})\.(\d{4})\b', spitedStr[0])):
+                        date_obj = datetime.strptime(spitedStr[0], '%d.%m.%Y')
+                        lastDate = date_obj.strftime('%Y-%m-%d')
+                        continue
+                    
+                    if datetime.strptime(lastDate, '%Y-%m-%d') <= previousDate:
+                        continue
+                    
+                    name = ''
+                    for namePart in spitedStr[0:-4:1]:
+                        name += namePart + ' '
+                    
+                    bufLine = self._createDict(
+                        shelf_id = 1,
+                        name = name,
+                        count = spitedStr[-2].replace(',000', ''),
+                        revenue = spitedStr[-1].replace(',', ''),
+                        date = lastDate,
+                    )
+                    
+                    readyLines.append(bufLine)
+                
+                if (len(readyLines) == 0):
+                    msg = f'Данные из письма: {title} уже содержатся в БД!'
+                    
+                    print(msg)
+                    self.logger.debug(msg)
+                else:
+                    msg = f'Письмо: {title} обработано успешно!'
+                    
+                    print(msg)
+                    self.logger.info(msg)
+                    
+            bufData = dict(self.js.getData())
+            
+            if (lastDate is not None):
+                bufData["polks"] = lastDate
+                self.js.writeData(bufData)
+                
+            return readyLines
+        except Exception as e:
+            msg = f'При сборе данных с Полкиуса произошла ошибка! [{e}]'
+                    
+            print(msg)
+            self.logger.error(msg)
+    
+    def CollectSalesFox(self):
+        try:
+            sender_email = 'lisyapolka@mail.ru'
+            
+            mails = self._GetMessagesFromGmail(sender_email, 1)
+            
+            readyLines = []
+            
+            lastDate = None
+            
+            for mail in mails:
+                title = mail.get_subject()
+                
+                msg = f'Обработка письма: {title}'
+                
+                print(msg)
+                self.logger.debug(msg)
+                
+                if not mail.html_part:
+                    msg = f'Письмо: {title} не содержит HTML'
+                    
+                    print(msg)
+                    self.logger.debug(msg)
+                    continue
+                
+                html = mail.html_part.get_payload().decode(mail.html_part.charset)
+                soup = BeautifulSoup(html, 'html.parser')
+                tables = soup.find_all('table')
+                
+                if not tables:
+                    msg = f'Нет таблиц в письме: {title}'
+                    
+                    print(msg)
+                    self.logger.warning(msg)
+                    continue
+                
+                rows = str(tables[3]).split('</tr>')
+                dateRow = rows[4]
+                rows = rows[5:-2]
+                
+                dateRow = re.sub(r'<.*?>', '', dateRow).strip()
+                dateRow = re.sub(r'\s+', ' ', dateRow).strip()
+                
+                lastDate = datetime.strptime(dateRow.split(' ')[0], '%d.%m.%Y').strftime('%Y-%m-%d')
+                
+                oldFoxDate = dict(self.js.getData())["fox"]
+                
+                if (oldFoxDate == 'None'):
+                    previousDate = datetime.strptime('2000-01-01', '%Y-%m-%d')
+                else:
+                    previousDate = datetime.strptime(oldFoxDate, '%Y-%m-%d')
+                
+                if (datetime.strptime(lastDate, '%Y-%m-%d') <= previousDate):
+                    msg = f'Данные из письма: {title} уже содержатся в БД!'
+                    
+                    print(msg)
+                    self.logger.debug(msg)
+                    continue
+                
+                for line in rows:
+                    cleaned_text = re.sub(r'<.*?>', '', line).strip()
+                    cleaned_text = re.sub(r'\s+', ' ', cleaned_text).strip()
+
+                    spitedStr = cleaned_text.split(' ')
+                    
+                    name = ''
+                    for namePart in spitedStr[1:-4:1]:
+                        name += namePart + ' '
+                    
+                    bufLine = self._createDict(
+                        shelf_id = 3,
+                        name = name,
+                        count = spitedStr[-4],
+                        revenue = spitedStr[-1],
+                        date = lastDate,
+                    )
+                    
+                    readyLines.append(bufLine)
+                    
+                msg = f'Письмо: {title} успешно обработано!'
                 
                 print(msg)
                 self.logger.info(msg)
-                
-        bufData = dict(self.js.getData())
-        
-        if (lastDate is not None):
-            bufData["polks"] = lastDate
-            self.js.writeData(bufData)
             
-        return readyLines
+            bufData = dict(self.js.getData())
+            
+            if (lastDate is not None):
+                bufData["fox"] = lastDate
+                self.js.writeData(bufData)
+            
+            return readyLines
+        
+        except Exception as e:
+            msg = f'При сборе данных с Лисьей полки произошла ошибка! [{e}]'
+                    
+            print(msg)
+            self.logger.error(msg)
     
-    def CollectSalesFox(self):
-        sender_email = 'lisyapolka@mail.ru'
-        
-        mails = self._GetMessagesFromGmail(sender_email, 1)
-        
-        readyLines = []
-        
-        lastDate = None
-        
-        for mail in mails:
-            title = mail.get_subject()
+    def CollectSalesWolf(self):
+        try:
+            dateTo = (datetime.now() - timedelta(days = 1)).strftime('%Y-%m-%d')
             
-            msg = f'Обработка письма: {title}'
-            
+            msg = f'Обработка сайта: Волчок {dateTo}'
+                
             print(msg)
             self.logger.debug(msg)
             
-            if not mail.html_part:
-                msg = f'Письмо: {title} не содержит HTML'
+            oldWolfDate = dict(self.js.getData())["wolf"]
+                
+            if (oldWolfDate == 'None'):
+                previousDate = datetime.strptime('2000-01-01', '%Y-%m-%d')
+            else:
+                previousDate = datetime.strptime(oldWolfDate, '%Y-%m-%d')
+                
+            if datetime.strptime(dateTo, '%Y-%m-%d') <= previousDate:
+                msg = f'Данные с сайта Волчок за {dateTo} уже содержатся в БД!'
                 
                 print(msg)
                 self.logger.debug(msg)
-                continue
+                return []
+
+            url = f'{self.urlPart}&dateFrom={dateTo}&dateTo={dateTo}'
+
+            response = requests.get(url)
+            soup = BeautifulSoup(response.text, 'html.parser')
+            table = soup.find('table', class_='table table-bordered', id='rows')
+
+            row = str(table).split('<tr>')
             
-            html = mail.html_part.get_payload().decode(mail.html_part.charset)
-            soup = BeautifulSoup(html, 'html.parser')
-            tables = soup.find_all('table')
-            
-            if not tables:
-                msg = f'Нет таблиц в письме: {title}'
+            if row[0] == 'None':
+                msg = f'Продажи у Волчока за {dateTo} отсутствуют!'
                 
                 print(msg)
                 self.logger.warning(msg)
-                continue
+                return []
             
-            rows = str(tables[3]).split('</tr>')
-            dateRow = rows[4]
-            rows = rows[5:-2]
+            row.pop(0)
+            row.pop(0)
+            row.pop(-1)
             
-            dateRow = re.sub(r'<.*?>', '', dateRow).strip()
-            dateRow = re.sub(r'\s+', ' ', dateRow).strip()
-            
-            lastDate = datetime.strptime(dateRow.split(' ')[0], '%d.%m.%Y').strftime('%Y-%m-%d')
-            
-            oldFoxDate = dict(self.js.getData())["fox"]
-            
-            if (oldFoxDate == 'None'):
-                previousDate = datetime.strptime('2000-01-01', '%Y-%m-%d')
-            else:
-                previousDate = datetime.strptime(oldFoxDate, '%Y-%m-%d')
-            
-            if (datetime.strptime(lastDate, '%Y-%m-%d') <= previousDate):
-                msg = f'Данные из письма: {title} уже содержатся в БД!'
-                
-                print(msg)
-                self.logger.debug(msg)
-                continue
-            
-            for line in rows:
+            readyLines = []
+
+            for line in row:
                 cleaned_text = re.sub(r'<.*?>', '', line).strip()
                 cleaned_text = re.sub(r'\s+', ' ', cleaned_text).strip()
-
+                
                 spitedStr = cleaned_text.split(' ')
                 
+                spitedStr.pop(0)
+                spitedStr.pop(0)
+                
                 name = ''
-                for namePart in spitedStr[1:-4:1]:
+                for namePart in spitedStr[0:-3:1]:
                     name += namePart + ' '
                 
                 bufLine = self._createDict(
-                    shelf_id = 3,
+                    shelf_id = 2,
                     name = name,
-                    count = spitedStr[-4],
+                    count = spitedStr[-3],
                     revenue = spitedStr[-1],
-                    date = lastDate,
-                )
+                    date = dateTo,
+                    )
                 
                 readyLines.append(bufLine)
                 
-            msg = f'Письмо: {title} успешно обработано!'
-            
-            print(msg)
-            self.logger.info(msg)
-           
-        bufData = dict(self.js.getData())
-        
-        if (lastDate is not None):
-            bufData["fox"] = lastDate
-            self.js.writeData(bufData)
-        
-        return readyLines
-    
-    def CollectSalesWolf(self):
-        dateTo = (datetime.now() - timedelta(days = 1)).strftime('%Y-%m-%d')
-        
-        msg = f'Обработка сайта: Волчок {dateTo}'
-            
-        print(msg)
-        self.logger.debug(msg)
-        
-        oldWolfDate = dict(self.js.getData())["wolf"]
-            
-        if (oldWolfDate == 'None'):
-            previousDate = datetime.strptime('2000-01-01', '%Y-%m-%d')
-        else:
-            previousDate = datetime.strptime(oldWolfDate, '%Y-%m-%d')
-            
-        if datetime.strptime(dateTo, '%Y-%m-%d') <= previousDate:
-            msg = f'Данные с сайта Волчок за {dateTo} уже содержатся в БД!'
+            msg = f'Данные с сайта Волчок за {dateTo} успешно собраны!'
             
             print(msg)
             self.logger.debug(msg)
-            return []
-
-        url = f'{self.urlPart}&dateFrom={dateTo}&dateTo={dateTo}'
-
-        response = requests.get(url)
-        soup = BeautifulSoup(response.text, 'html.parser')
-        table = soup.find('table', class_='table table-bordered', id='rows')
-
-        row = str(table).split('<tr>')
-        
-        if row[0] == 'None':
-            msg = f'Продажи у Волчока за {dateTo} отсутствуют!'
             
+            bufData = dict(self.js.getData())
+            bufData["wolf"] = dateTo
+            self.js.writeData(bufData)
+            
+            return readyLines
+        
+        except Exception as e:
+            msg = f'При сборе данных с Волчка произошла ошибка! [{e}]'
+                    
             print(msg)
-            self.logger.warning(msg)
-            return []
-        
-        row.pop(0)
-        row.pop(0)
-        row.pop(-1)
-        
-        readyLines = []
-
-        for line in row:
-            cleaned_text = re.sub(r'<.*?>', '', line).strip()
-            cleaned_text = re.sub(r'\s+', ' ', cleaned_text).strip()
-            
-            spitedStr = cleaned_text.split(' ')
-            
-            spitedStr.pop(0)
-            spitedStr.pop(0)
-            
-            name = ''
-            for namePart in spitedStr[0:-3:1]:
-                name += namePart + ' '
-            
-            bufLine = self._createDict(
-                shelf_id = 2,
-                name = name,
-                count = spitedStr[-3],
-                revenue = spitedStr[-1],
-                date = dateTo,
-                )
-            
-            readyLines.append(bufLine)
-            
-        msg = f'Данные с сайта Волчок за {dateTo} успешно собраны!'
-        
-        print(msg)
-        self.logger.debug(msg)
-        
-        bufData = dict(self.js.getData())
-        bufData["wolf"] = dateTo
-        self.js.writeData(bufData)
-        
-        return readyLines
+            self.logger.error(msg)
     
     def _createDict(self, shelf_id, name, count, revenue, date):
         bufLine = {
